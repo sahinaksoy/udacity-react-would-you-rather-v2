f8bd0cc6d18e22b0f035f0ef6acb7d568276329d
sahinaksoy <sahinaksoy@gmail.com>
2020-07-27 23:05:05 +0300

    feat: add new pool design

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,10 +1,64 @@
 import React, { Component } from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Input,
+} from "reactstrap";
+import { Button } from "semantic-ui-react";
 
 class NewQuestion extends Component {
+  state = {
+    optionOne: "",
+    optionTwo: "",
+  };
+
+  handleChange = (e) => {
+    
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
   render() {
-    return <div>
-        Welcome new question page
-    </div>;
+    return (
+      <section style={{ marginTop: 15 }}>
+        <Card>
+          <CardHeader>New pool</CardHeader>
+          <CardBody>
+            <CardTitle>Would you rather...</CardTitle>
+            <Input
+              type="text"
+              name="optionOne"
+              id="optionOne"
+              placeholder="Type option one"
+              onChange={this.handleChange}
+            />
+            <hr />
+            <Input
+              type="text"
+              name="optionTwo"
+              id="optionTwo"
+              placeholder="Type option two"
+              onChange={this.handleChange}
+            />
+            <br />
+            <Button
+              primary
+              disabled={
+                this.state.optionOne == "" || this.state.optionTwo == ""
+              }
+              onClick={() => {
+                //TODO: handle add new question
+                console.log(this.state);
+              }}
+            >
+              Submit
+            </Button>
+          </CardBody>
+        </Card>
+      </section>
+    );
   }
 }
 
