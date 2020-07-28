09ca6c9f31e3c31ee405947cecad1045845f481f
sahinaksoy <sahinaksoy@gmail.com>
2020-07-28 13:15:09 +0300

    feat: add table to leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import { Card, CardHeader, CardBody, Table } from "reactstrap";
-
+import { connect } from "react-redux";
 class Leaderboard extends Component {
   render() {
+    const { leaderBoard } = this.props;
     return (
       <section style={{ marginTop: 15 }}>
         <Card>
@@ -12,6 +13,7 @@ class Leaderboard extends Component {
               <thead>
                 <tr>
                   <th>#</th>
+                  <th>Image</th>
                   <th>Author</th>
                   <th>Question</th>
                   <th>Answer</th>
@@ -19,13 +21,18 @@ class Leaderboard extends Component {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>
+                {leaderBoard.map((user, index) => (
+                  <tr>
+                    <th scope="row">{++index}</th>
+                    <td>
+                      {<img src={user.avatarURL} style={{ width: 100 }} />}
+                    </td>
+                    <td>{user.name}</td>
+                    <td>{user.questionCount}</td>
+                    <td>{user.answerCount}</td>
+                    <td>{user.total}</td>
+                  </tr>
+                ))}
               </tbody>
             </Table>
           </CardBody>
@@ -35,4 +42,23 @@ class Leaderboard extends Component {
   }
 }
 
-export default Leaderboard;
+const mapStateToProps = (state) => {
+  const { users } = state;
+  const leaderBoard = Object.values(users)
+    .map((user) => ({
+      id: user.id,
+      name: user.name,
+      avatarURL: user.avatarURL,
+      answerCount: Object.values(user.answers).length,
+      questionCount: user.questions.length,
+      total: Object.values(user.answers).length + user.questions.length,
+    }))
+    .sort((a, b) => a.total - b.total)
+    .reverse()
+    .slice(0, 3);
+  return {
+    leaderBoard,
+  };
+};
+
+export default connect(mapStateToProps)(Leaderboard);
