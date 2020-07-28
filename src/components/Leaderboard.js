855d028a79f27af4e3be9db7c25b7702789e3ff2
sahinaksoy <sahinaksoy@gmail.com>
2020-07-28 12:40:27 +0300

    feat: add table to leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,8 +1,37 @@
 import React, { Component } from "react";
+import { Card, CardHeader, CardBody, Table } from "reactstrap";
 
 class Leaderboard extends Component {
   render() {
-    return <div>Welcome to Leaderboard</div>;
+    return (
+      <section style={{ marginTop: 15 }}>
+        <Card>
+          <CardHeader>Leaderboard</CardHeader>
+          <CardBody>
+            <Table>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Author</th>
+                  <th>Question</th>
+                  <th>Answer</th>
+                  <th>Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>1</td>
+                  <td>2</td>
+                  <td>3</td>
+                </tr>
+              </tbody>
+            </Table>
+          </CardBody>
+        </Card>
+      </section>
+    );
   }
 }
 
